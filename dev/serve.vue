a5c7d4114b5e12a8ec7f16097e1d5b6b187ecba1
codeeshop-oc <anantnegi8@gmail.com>
2021-07-10 16:03:03 +0530

    inital

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import { defineComponent } from 'vue';
+import Clipboard from '@/clipboard.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    Clipboard
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <clipboard />
+  </div>
+</template>
