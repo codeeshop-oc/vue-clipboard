079c6783e2f3eb3764a94af34fc3c51cf7e25812
codeeshop-oc <anantnegi8@gmail.com>
2021-07-10 17:32:45 +0530

    initial updates

--- src/Clipboard.vue
+++ src/Clipboard.vue
@@ -10,8 +10,6 @@
 </template>
 
 <script>
-  import '@/style.css';
-
   export default ({
     name: 'Clipboard',
     props: {
@@ -99,4 +97,114 @@
 </script>
 
 <style scoped>
+.disabled {
+  pointer-events: none;
+}
+.pointer {
+  cursor: pointer;
+}
+.tgl {
+  display: none;
+}
+.tgl,
+.tgl:after,
+.tgl:before,
+.tgl *,
+.tgl *:after,
+.tgl *:before,
+.tgl+.tgl-btn {
+  box-sizing: border-box;
+}
+.tgl::-moz-selection,
+.tgl:after::-moz-selection,
+.tgl:before::-moz-selection,
+.tgl *::-moz-selection,
+.tgl *:after::-moz-selection,
+.tgl *:before::-moz-selection,
+.tgl+.tgl-btn::-moz-selection {
+  background: none;
+}
+.tgl::selection,
+.tgl:after::selection,
+.tgl:before::selection,
+.tgl *::selection,
+.tgl *:after::selection,
+.tgl *:before::selection,
+.tgl+.tgl-btn::selection {
+  background: none;
+}
+.tgl+.tgl-btn {
+  outline: 0;
+  display: block;
+  width: 4em;
+  height: 2em;
+  position: relative;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+.tgl+.tgl-btn:after,
+.tgl+.tgl-btn:before {
+  position: relative;
+  display: block;
+  content: "";
+  width: 50%;
+  height: 100%;
+}
+.tgl+.tgl-btn:after {
+  left: 0;
+}
+.tgl+.tgl-btn:before {
+  display: none;
+}
+.tgl-skewed+.tgl-btn {
+  border-radius: 5px;
+  padding: 1px 10px;
+  overflow: hidden;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  transition: all 0.2s ease;
+  font-family: sans-serif;
+  background: #888;
+}
+.tgl-skewed+.tgl-btn:after,
+.tgl-skewed+.tgl-btn:before {
+  display: inline-block;
+  transition: all 0.2s ease;
+  width: 100%;
+  text-align: center;
+  position: absolute;
+  line-height: 2em;
+  font-weight: bold;
+  color: #fff;
+  text-shadow: 0 1px 0 rgba(0, 0, 0, 0.4);
+}
+.tgl-skewed+.tgl-btn:after {
+  left: 100%;
+  content: attr(data-tg-on);
+}
+.tgl-skewed+.tgl-btn:before {
+  left: 0;
+  content: attr(data-tg-off);
+}
+.tgl-skewed+.tgl-btn:active {
+  background: #888;
+}
+.tgl-skewed+.tgl-btn:active:before {
+  left: -10%;
+}
+.tgl-skewed:checked+.tgl-btn {
+  background: #86d993;
+}
+.tgl-skewed:checked+.tgl-btn:before {
+  left: -100%;
+}
+.tgl-skewed:checked+.tgl-btn:after {
+  left: 0;
+}
+.tgl-skewed:checked+.tgl-btn:active:after {
+  left: 10%;
+}
 </style>
